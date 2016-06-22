b97ef2f96caeafb47e3c48bea7ef5a4ffe402b89
Li4dz <richard.rggdev@gmail.com>
2016-06-21 23:42:19 -0500

    Commit

--- controller.js
+++ controller.js
@@ -5,15 +5,15 @@ app.controller("FirstController", function($scope)
 	$scope.nombre = "Richard";
     $scope.nuevoComentario = {};
     $scope.comentarios = [
-    // {
-    //     comentario : "Buen Trabjao",
-    //     username : "Usuario 1"
-    // },
-    // {
-    //     comentario : "Mal Trabajo",
-    //     username : "Usuario 2"
-    // }
-    
+    {
+        comentario : "Buen Trabjao",
+        username : "Usuario 1"
+    },
+    {
+        comentario : "Mal Trabajo",
+        username : "Usuario 2"
+    }
+
     ];
 
     $scope.agregarComentario = function() 
